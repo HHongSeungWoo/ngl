ca2453351c670faf0b35e1919350d2f332811818
Hong Seungwoo <qksn1541@gmail.com>
2023-07-23 16:54:05 +0900

    Loader (#2)
    * remove: promise-polyfill
    
    * remove: buble compiler
    
    * remove: polyfills.js
    
    * lint: .
    
    * build: .
    
    * remove: unused plugin
    
    * chore: update plugin-terser
    
    * build: .
    
    * fix build

--- rollup.config.dist.js
+++ rollup.config.dist.js
@@ -1,6 +1,5 @@
 import terser from '@rollup/plugin-terser';
 import { plugins, moduleGlobals, umdGlobals, moduleExternals } from './rollup.config.js'
-import { threeMinifier } from "@yushijinhun/three-minifier-rollup";
 
 const minModuleConfig = {
   input: 'src/ngl.ts',
@@ -23,17 +22,5 @@ const minModuleConfig = {
   external: moduleExternals
 }
 
-const minBundleConfig = {
-  input: 'src/ngl.ts',
-  plugins: [ threeMinifier(), ...plugins, terser()],
-  output: {
-    file: 'dist/ngl.js',
-    format: 'umd',
-    name: 'NGL',
-    sourcemap: true,
-    globals: {}
-  },
-  external: []
-}
 
-export default [ minModuleConfig, minBundleConfig ]
+export default [ minModuleConfig ]
